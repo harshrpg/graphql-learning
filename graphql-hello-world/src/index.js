e819279f825caf46968e3503e29322b72b469226
harshrpg <harshrp94@gmail.com>
2019-11-24 13:45:43 +0000

    P2: Basic user types

--- graphql-hello-world/src/index.js
+++ graphql-hello-world/src/index.js
@@ -1,17 +1,63 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
+
 
 const typeDefs = gql`
-    type Query {
-        hello: String!
-    }
+  type Query {
+    hello: String
+    user: User
+  }
+  type User {
+    id: ID!
+    username: String!
+  }
+  type Error {
+    field: String!
+    message: String!
+  }
+  type RegisterResponse {
+    errors: [Error!]!
+    user: User
+  }
+  input UserInfo {
+    username: String!
+    password: String!
+    age: Int
+  }
+  type Mutation {
+    register(userInfo: UserInfo!): RegisterResponse!
+    login(userInfo: UserInfo!): Boolean!
+  }
 `;
 
 const resolvers = {
-    Query: {
-        hello: () => "hello world"
-    }
-}
+  Query: {
+    hello: () => null,
+    user: () => ({
+      id: 1,
+      username: "bob"
+    })
+  },
+  Mutation: {
+    login: () => true,
+    register: () => ({
+      errors: [
+        {
+          field: "username",
+          message: "bad"
+        },
+        {
+          field: "username2",
+          message: "bad2"
+        }
+      ],
+      user: {
+        id: 1,
+        username: "bob"
+      }
+    })
+  }
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({url}) => console.log(`server started at ${url}`))
+server.listen().then(({ url }) => console.log(`server started at ${url}`));
